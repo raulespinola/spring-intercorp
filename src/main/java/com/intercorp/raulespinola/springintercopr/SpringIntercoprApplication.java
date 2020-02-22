134b745335f123ab3586aeb4339ef746fb7ad4df
Raul <raulespinola@gmail.com>
2020-02-22 16:33:59 -0300

    Initialize Repository

---
+++ src/main/java/com/intercorp/raulespinola/springintercopr/SpringIntercoprApplication.java
@@ -0,0 +1,13 @@
+package com.intercorp.raulespinola.springintercopr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntercoprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntercoprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intercorp/raulespinola/springintercopr/SpringIntercoprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intercorp.raulespinola.springintercopr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntercoprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
