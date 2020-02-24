6af26910803147d5b873ae464cca2b11f2f2e578
Raul <raulespinola@gmail.com>
2020-02-24 11:18:08 -0300

    Add Rest Services + Stadistical

--- src/main/java/com/intercorp/raulespinola/intercorp/controller/CustomerController.java
+++ src/main/java/com/intercorp/raulespinola/intercorp/controller/CustomerController.java
@@ -4,7 +4,11 @@ import com.intercorp.raulespinola.intercorp.domain.Customer;
 import com.intercorp.raulespinola.intercorp.repositories.CustomerRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customers")
@@ -38,4 +42,68 @@ public class CustomerController {
     public void delete(@PathVariable("id") String id){
         this.customerRepository.deleteById(id);
     }
+
+    // Find Customer by Id
+    @GetMapping("/{id}")
+    public Optional<Customer> getById(@PathVariable("id") String id){
+        return this.customerRepository.findById(id);
+    }
+
+    // List all clients
+    @GetMapping("/listclientes")
+    public List<Customer> getAllClientsWithDeadDate(){
+
+        return this.customerRepository.findAll();
+    }
+
+    // List all clients
+    @GetMapping("/kpidclientes")
+    public List<Double> getAverageAndDeviation(){
+
+        List<Customer> customerList = this.customerRepository.findAll();
+                // mean
+        double mean = customerList.stream()
+                .mapToInt(Customer::getAge)
+                .average()
+                .getAsDouble();
+
+        System.out.println("Medio" + mean );
+
+
+        // Variance
+        double variance = customerList.stream()
+                .map(i -> {
+                    return (i.getAge() - mean);
+                })
+                .map(i -> i*i)
+                .mapToDouble(i -> i).average().getAsDouble();
+
+        System.out.println("Varianza" + variance);
+
+        //Standard Deviation
+        double standardDeviation = Math.sqrt(variance);
+        System.out.println("Desviacion" + standardDeviation);
+
+
+        double average = customerList.stream()
+                .mapToInt(Customer::getAge)
+                .summaryStatistics()
+                .getAverage();
+
+        List <Double> out = Arrays.asList(average, variance, standardDeviation);
+
+        return out;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
