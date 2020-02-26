b1348de26438bd5ea66e256b42d888722416ca8f
Raul <raulespinola@gmail.com>
2020-02-26 12:23:53 -0300

    Add Swagger Support

---
+++ src/main/java/com/intercorp/raulespinola/intercorp/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.intercorp.raulespinola.intercorp.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.intercorp.raulespinola.intercorp.web.controller"))
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("Rest API", "Rest API Customer", "1", "",
+                new Contact("Raul Espinola", "www.raulespinola.wordpress.com", "raulespinola@gmail.com"),
+                "Apache License", "");
+        return apiInfo;
+    }
+}
--- src/test/java/com/intercorp/raulespinola/springintercopr/SpringIntercorpApplicationTests.java
+++ src/test/java/com/intercorp/raulespinola/springintercopr/SpringIntercorpApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringIntercoprApplicationTests {
+class SpringIntercorpApplicationTests {
 
 	@Test
 	void contextLoads() {
